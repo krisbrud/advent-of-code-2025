c68bddd53f67325b5291b47d8ff2f7949ac490fd
Kristian Brudeli <krbrudeli@gmail.com>
2025-12-05 21:43:40 +0100

    Solve day 4 part 1

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,7 +1,105 @@
 advent_of_code::solution!(4);
 
+use itertools::Itertools;
+
+type Coord = (usize, usize);
+
+struct Board {
+    tiles: Vec<Vec<char>>,
+    rows: usize,
+    cols: usize,
+}
+
+const FREE_TILE: char = '.';
+const ROLL_TILE: char = '@';
+
+impl Board {
+    fn new(s: &str) -> Option<Board> {
+        let plants: Vec<Vec<char>> = s.lines().map(|line| line.chars().collect()).collect();
+
+        let rows = s.lines().collect_vec().len();
+        let cols = s.lines().nth(0).map(|x| x.len())?;
+
+        Some(Board {
+            tiles: plants,
+            rows,
+            cols,
+        })
+    }
+
+    // Assume bounds check has already been done
+    fn get(&self, coordinate: &Coord) -> char {
+        self.tiles[coordinate.0][coordinate.1]
+    }
+
+    fn maybe_tile(&self, coordinate: &Coord) -> Option<char> {
+        if coordinate.0 < self.rows && coordinate.1 < self.cols {
+            Some(self.get(coordinate))
+        } else {
+            None
+        }
+    }
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    let board = Board::new(input)?;
+
+    let mut accessible = 0;
+
+    for r in 0..board.rows as i32 {
+        // println!("foo");
+        for c in 0..board.cols as i32 {
+            if board.get(&(r as usize, c as usize)) != ROLL_TILE {
+                // Not a roll, no need to count
+                continue;
+            }
+            let mut occupied_adj = 0;
+            for dr in -1i32..=1 {
+                // println!("baz");
+                for dc in -1i32..=1 {
+                    // println!("ham");
+                    if dr == 0 && dc == 0 {
+                        continue; // Same roll
+                    }
+
+                    let maybe_row = r + dr;
+                    let maybe_col = c + dc;
+
+                    if maybe_row < 0 || maybe_col < 0 {
+                        continue;
+                    }
+
+                    let row = maybe_row as usize;
+                    let col = maybe_col as usize;
+                    let coordinate = (row, col);
+
+                    if let Some(neigh) = board.maybe_tile(&coordinate) {
+                        // println!(
+                        //     "coord: {:?} neigh {:?} tile: {:?}",
+                        //     (r, c),
+                        //     (row, col),
+                        //     tile
+                        // );
+                        if neigh != FREE_TILE {
+                            occupied_adj += 1;
+                        }
+                        // Otherwise free
+                    } else {
+                        // Out of board, don't count
+                    }
+                }
+            }
+
+            // let free_adj = 8 - occupied_adj;
+            // println!("free adjacent: {}", free_adj);
+            if occupied_adj < 4 {
+                println!("available: {:?}", (r, c));
+                accessible += 1;
+            }
+        }
+    }
+
+    Some(accessible)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
@@ -15,7 +113,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(13));
     }
 
     #[test]
